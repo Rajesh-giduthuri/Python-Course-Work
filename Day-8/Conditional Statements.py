469de6b1415407077f5c85ea34c70e7e4b0640f6
Rajesh-giduthuri <rajeshgiduthuri28@gmail.com>
2026-09-17 09:33:42 +0530

    Added Day-8 Python

---
+++ Day-8/Conditional Statements.py
@@ -0,0 +1,35 @@
+#Control Statements
+#conditional statements
+
+#simple if
+n=int(input())
+if n>0:
+    print("positive")
+
+#if else
+age=int(input())
+if age>18:
+    print("Eligible")
+else:
+    print("Not Eligible")
+
+#if elif else
+n=int(input())
+if n>0:
+    print("+ve")
+elif n<0:
+    print("-ve")
+else:
+    print("Zero")
+
+#Nested if
+marks=int(input())
+attendance=int(input())
+if marks >= 90:
+    print("Marks requirement met.")
+    if attendance >= 85:
+        print("Scholarship Approved!")  
+    else:
+        print("Attendance requirement not met.")
+else:
+    print("Marks requirement not met.")
