16121b71127efe3aaadce82911fe509f5639646f
Rajesh-giduthuri <rajeshgiduthuri28@gmail.com>
2026-09-17 09:31:15 +0530

    Added Day-4 Python

---
+++ Day-4/Format String.py
@@ -0,0 +1,10 @@
+#formate string or F string 
+name = "Sreekanth"
+age = 22
+print(f"Hello, my name is {name} and I am {age} years old.")
+print(f"my percentage is {90.5:.2f}%") #formatting to 2 decimal places
+
+#Dot formatting
+name = "Sreekanth"
+age = 22
+print("Hello, my name is {name} and I am {age} years old.".format(name=name, age=age))
---
+++ Day-4/Identity Operator.py
@@ -0,0 +1,10 @@
+#identity operator
+# is , not is 
+
+a=[10,20,30]
+b=a
+print(a is b) #reference is same
+
+a=[10,20,30]
+b=[10,20,30]
+print(a is b) # reference is different
