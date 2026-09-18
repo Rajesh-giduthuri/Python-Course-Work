5beacc04417c70f10221ea39c44cf0728333aaa3
Rajesh-giduthuri <rajeshgiduthuri28@gmail.com>
2026-09-18 21:10:56 +0530

    Added Day-18 Python

---
+++ Day-18/List Comprehension.py
@@ -0,0 +1,6 @@
+#read only even numbers from a list using list comprehension
+numbers = [1, 2, 3, 4, 5, 6]
+even_numbers = [x for x in numbers if x % 2 == 0]
+print(even_numbers)
+
+#output:[2, 4, 6]
