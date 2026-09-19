a57e36171f8275500c46b6548a6a5cffb7bbee36
Rajesh-giduthuri <rajeshgiduthuri28@gmail.com>
2026-09-19 11:22:04 +0530

    Files Renamed

--- Day-4/Comparision Operator.py
+++
@@ -1,9 +0,0 @@
-#comparision operator
-a=10
-b=20
-print(a==b)  # equal to
-print(a!=b)  # not equal to
-print(a>b)  # greater than
-print(a<b)  # less than
-print(a>=b)  # greater than or equal to
-print(a<=b)  # less than or equal to
--- Day-9/Problems on conditional statements.py
+++
@@ -1,9 +0,0 @@
-#even or odd
-n=int(input())
-
-if n%2==0:
-    print("Even")
-else:
-    print("Odd")
-
-
